8759b501f20399512dd2c1712cdf9990e111b651
MicroNote Coin <31553093+micronotecoin@users.noreply.github.com>
2017-09-13 01:35:20 +0200

    Add files via upload

---
+++ website_example/config.js
@@ -0,0 +1,11 @@
+var api = "http://127.0.0.1:8117";
+
+var coinUnits = 1000000000000;
+
+var poolHost = "yourhost.com";
+
+var email = "support@domain.com";
+
+var blockchainExplorer = "http://blocks.unotecoin.com/block/";
+
+var transactionExplorer = "http://blocks.unotecoin.com/transaction/";
